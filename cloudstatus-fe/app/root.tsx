c2ef98815c84a45f8b5b6c93c8d30a4b244660e9
zjyl1994 <me@zjyl1994.com>
2025-04-13 22:26:53 +0800

    feat: update webui lang

--- cloudstatus-fe/app/root.tsx
+++ cloudstatus-fe/app/root.tsx
@@ -31,7 +31,7 @@ export const links: Route.LinksFunction = () => [
 
 export function Layout({ children, title }: { children: React.ReactNode; title: string }) {
   return (
-    <html lang="en">
+    <html lang="zh-CN">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
